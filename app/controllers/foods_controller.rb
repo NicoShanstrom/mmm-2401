429dd667d8b23d2242a732b8684aea7cf9d18b93
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-27 13:54:41 -0400

    Create facade test, and modify facade search method to create objects from the response before passing to controller action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,7 +2,7 @@ class FoodsController < ApplicationController
 
     def show
       @searched_food = params[:q]
-      foods_data = FoodDataApiFacade.search_foods(@searched_food)
-      @foods = FoodService.build_foods(foods_data['foods'])
+      @foods = FoodDataApiFacade.search_foods(@searched_food)
+      # @foods = FoodService.build_foods(foods_data['foods'])
     end
   end
--- app/facades/food_data_api_facade.rb
+++ app/facades/food_data_api_facade.rb
@@ -4,5 +4,7 @@ class FoodDataApiFacade
     endpoint = "/fdc/v1/foods/search"
     params = { query: term, pageSize: 10 }
     response = FoodDataApi.get(endpoint, params)
+    require 'pry'; binding.pry
+    FoodService.build_foods(response['foods'])
   end
 end
